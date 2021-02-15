dbb44bf3f595aac9f473a0f1a8d9f50befb7d45a
David Wasilewko <david.wasilewko@jaemacom.de>
2021-02-15 17:17:05 +0100

    Add AOP Project

---
+++ AopTest/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Norns.Urd.Interceptors;
+using Norns.Urd.Attributes;
+
+namespace AopTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(nameof(Test))]
+        public async Task<IActionResult> Test()
+        {
+            DoStuff();
+            return new OkObjectResult(true);
+        }
+
+
+        [TestInterceptor]
+        public void DoStuff()
+        {
+            Console.WriteLine("I do stuff");
+        }
+    }
+}
---
+++ AopTest/TestInterceptorAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Norns.Urd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AopTest
+{
+    public class TestInterceptorAttribute : AbstractInterceptorAttribute
+    {
+        public override async Task InvokeAsync(AspectContext context, AsyncAspectDelegate next)
+        {
+            Console.WriteLine("This is the interceptor talking!");
+            await next(context);
+            Console.WriteLine("This is the interceptor talking again!");
+        }
+    }
+}
